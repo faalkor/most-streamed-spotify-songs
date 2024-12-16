666a41e0cae894a683234ff1b4ff0b3f32ad184a
yPabloo <pablo.afreire@gmail.com>
2024-12-15 23:42:34 -0300

    :sparkles: feat: Novos graficos e tabelas

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import chardet
 import os
 from graph import graph
+import matplotlib.pyplot as plt
 
 country_path = os.path.join(os.getcwd(), 'app', 'countrys.json')
 
@@ -50,3 +51,45 @@ st.write(df)
 # Graphs
 st.markdown("### Popularidade por streaming")
 graph(df)
+
+# Tabela de contagem de artistas
+st.markdown("## Artistas e Frequência de Aparição")
+
+# Contar a quantidade de vezes que cada artista aparece
+artist_count = df['Artist'].value_counts().reset_index()
+artist_count.columns = ['Artista', 'Quantidade de Aparição']
+
+# Exibir a tabela no dashboard
+st.dataframe(artist_count)
+
+
+
+
+# Layout em colunas
+col1, col2 = st.columns(2)
+
+# Coluna 1: Gráfico de barras
+with col1:
+
+    # Converter a coluna 'Release Date' para datetime
+    df['Release Date'] = pd.to_datetime(df['Release Date'], errors='coerce')
+
+    # Criar uma nova coluna 'Year' apenas com o ano
+    df['Year'] = df['Release Date'].dt.year
+
+    # Contar a quantidade de músicas por ano
+    year_count = df['Year'].value_counts().reset_index()
+    year_count.columns = ['Year', 'Count']
+
+    # Gráfico de pizza no Streamlit
+    st.markdown("## Distribuição de Músicas por Ano")
+    fig, ax = plt.subplots()
+    ax.bar(year_count['Year'], year_count['Count'], color='skyblue')
+    ax.set_xlabel("Ano")
+    ax.set_ylabel("Quantidade de Músicas")
+    ax.set_title("Quantidade de Músicas por Ano")
+    plt.xticks(rotation=45)  # Rotaciona os anos para melhorar a visualização
+    st.pyplot(fig)
+
+with col2:
+    st.dataframe(year_count)
